c8e4a627e837fa07309e5a330974cd8480664caf
GrzesiekKozdroj <larhendiel@yahoo.co.uk>
2021-04-17 23:14:05 +0100

    added: nodemon, not configured with frontend yet.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const PORT = process.env.PORT || 4200
 
 app.use(   express.static(  path.join( __dirname, 'build' )  )    )
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'build','index.html'))
 })
-app.listen(3000)
+app.listen(PORT)
